97647dbc71a112da22d9069684311ca1be8bb6bc
Issa <isasarhan.lz@gmail.com>
2023-01-03 00:50:53 +0200

    adding '0.0.0.0  2'

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const dotenv = require('dotenv')
 const logger = require('./start/logger')
 const express = require('express')
+const PORT = process.env.PORT || 5000;
+const app = express()
 
 
 dotenv.config()
-const app = express()
 
 require('./start/db')()
 require('./start/routes')(app)
 require('./start/validation')()
 require('./start/prod')(app)
 
-app.listen(process.env.PORT || 2000,'0.0.0.0',
-    () => logger.info(`listening to port ${process.env.PORT}...`))
+
+app.listen(PORT,'0.0.0.0',
+    () => logger.info(`listening to port ${PORT}...`))
 
