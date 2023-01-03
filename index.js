29d20ac190c507e639f1b6a9566135c949e4b894
Issa <isasarhan.lz@gmail.com>
2023-01-03 12:21:04 +0200

    adding cors

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const dotenv = require('dotenv')
 const logger = require('./start/logger')
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 dotenv.config()
 
-const PORT = process.env.PORT || 2000;
-
-require('./start/db')()
+app.use(cors())
 require('./start/routes')(app)
+require('./start/db')()
 require('./start/validation')()
 require('./start/prod')(app)
 
 
+const PORT = process.env.PORT || 2000;
 app.listen(PORT, '0.0.0.0',
     () => logger.info(`listening to port ${PORT}...`))
 
