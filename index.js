a9f9b07626af030d75b16e6f56fd62d96a8f5a73
Issa <isasarhan.lz@gmail.com>
2023-01-11 12:27:54 +0200

    testing without nodemon

--- index.js
+++ index.js
@@ -5,27 +5,17 @@ const dotenv = require('dotenv')
 const cors = require('cors')
 
 
-const userRouter = require('./routes/user')
 const customerRouter = require('./routes/customer')
-const authRouter = require('./routes/auth')
-const orderRouter = require('./routes/order')
-const employeeRouter = require('./routes/employee')
-const balanceRouter = require('./routes/balance')
-const attendenceRouter = require('./routes/attendence')
+
 dotenv.config()
 
 app.use(cors())
 require('./start/db')()
+app.use(express.json())
 
-app.use('/api/attendence/', attendenceRouter)
-app.use('/api/auth/', authRouter)
-app.use('/api/balance/', balanceRouter)
 app.use('/api/customers/', customerRouter)
-app.use('/api/employees/', employeeRouter)
-app.use('/api/orders/', orderRouter)
-app.use('/api/users/', userRouter)
-require('./start/validation')()
 
+require('./start/validation')()
 
 const PORT = process.env.PORT || 2000;
 app.listen(PORT, '0.0.0.0',
