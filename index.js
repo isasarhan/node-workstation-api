b35fdfbe2c0a710185f914717308ddc42fdca626
Issa <isasarhan.lz@gmail.com>
2023-01-12 12:08:42 +0200

    testing  refractoring

--- index.js
+++ index.js
@@ -3,25 +3,22 @@ const app = express()
 const logger = require('./start/logger')
 const dotenv = require('dotenv')
 const cors = require('cors')
-const mongoose = require('mongoose')
 
 const customerRouter = require('./routes/customer')
 const balanceRouter = require('./routes/balance')
 
 dotenv.config()
-mongoose.set('strictQuery', false);
-    mongoose.connect(process.env.db)
-        .then(() => logger.info('connecting to database...'))
 
-// app.use(cors())
-// require('./start/db')()
-// require('./start/routes')(app)
-
-app.use('/api/customers/', customerRouter)
-app.use('/api/balance/', balanceRouter)
+app.use(cors())
+require('./start/db')()
+require('./start/validation')()
+require('./start/routes')(app)
 
 require('./start/prod')(app)
-require('./start/validation')()
+
+// app.use('/api/customers/', customerRouter)
+// app.use('/api/balance/', balanceRouter)
+
 
 const PORT = process.env.PORT || 2000;
 app.listen(PORT, '0.0.0.0',
