e6c2b79312a5db4e8b08df9f8981a57f0925b28b
Issa <isasarhan.lz@gmail.com>
2023-01-14 18:32:40 +0200

    ali

--- index.js
+++ index.js
@@ -35,19 +35,33 @@ const express = require('express')
 
 
 
-const app = express();
+
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// import "./lib/db";
+const express_1 = __importDefault(require("express"));
+// import countryRoutes from "./routes/country";
+const app = (0, express_1.default)();
 const port = process.env.PORT || 3333;
-
-app.use(express.json());
-app.use(express.raw({ type: "application/vnd.custom-type" }));
-app.use(express.text({ type: "text/html" }));
-
-app.get("/", async (req, res) => {
-  res.json({ message: "Please visit /countries to view all the countries" });
-});
-
-app.use("/countries", countryRoutes);
-
+app.use(express_1.default.json());
+app.use(express_1.default.raw({ type: "application/vnd.custom-type" }));
+app.use(express_1.default.text({ type: "text/html" }));
+app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    res.json({ message: "Testing" });
+}));
+// app.use("/countries", countryRoutes);
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
