237709645807f38cb84318d8a31d0e4e86da6a47
Issa <isasarhan.lz@gmail.com>
2023-01-14 18:10:28 +0200

    ali

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const logger = require('./start/logger')
+// const logger = require('./start/logger')
 // const dotenv = require('dotenv')
 const cors = require('cors')
 const app = express()
@@ -18,16 +18,17 @@ app.get('/', async (req, res) => {
 require('./start/validation')()
 // require('./start/prod')(app)
 
-const PORT = process.env.PORT || 2000;
-app.listen(PORT, '0.0.0.0',
-() => logger.info(`listening to port ${PORT}...`))
+const PORT = process.env.PORT || 3333;
+
+// app.listen(PORT, () => logger.info(`listening to port ${PORT}...`))
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
 
 
 
-
-
-const customerRouter = require('./routes/customer')
-const balanceRouter = require('./routes/balance')
-const { Customer } = require('./models/Customer')
-    // app.use('/api/customers/', customerRouter)
-    // app.use('/api/balance/', balanceRouter)
+// const customerRouter = require('./routes/customer')
+// const balanceRouter = require('./routes/balance')
+// const { Customer } = require('./models/Customer')
+//     // app.use('/api/customers/', customerRouter)
+//     // app.use('/api/balance/', balanceRouter)s
